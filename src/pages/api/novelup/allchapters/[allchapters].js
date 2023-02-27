15324d60de147f8eb8515d77ef45136fecd966f2
hellboy41785 <unknown41785@gmail.com>
2023-02-27 15:03:33 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/pages/api/novelup/allchapters/[allchapters].js

--- src/pages/api/novelup/allchapters/[allchapters].js
+++ src/pages/api/novelup/allchapters/[allchapters].js
@@ -1,29 +1,21 @@
 const fs = require("fs");
 
+const puppeteer = require('puppeteer-extra')
+
 const StealthPlugin = require("puppeteer-extra-plugin-stealth");
-const { executablePath } = require("puppeteer");
+const {executablePath} = require('puppeteer')
+
+
 
 const url = `https://www.novelupdates.com/series`;
-let chrome = {};
-let puppeteer;
 
-if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-  // running on the Vercel platform.
-  chrome = require("chrome-aws-lambda");
-  puppeteer = require("puppeteer-core");
-} else {
-  // running locally.
-  puppeteer = require("puppeteer-extra");
-}
 const scrapeData = async ({ slug }) => {
   try {
     await puppeteer.use(StealthPlugin());
     const browser = await puppeteer.launch({
-      args: [...chrome.args, '--hide-scrollbars', '--disable-web-security'],
-      defaultViewport: chrome.defaultViewport,
-      executablePath: await chrome.executablePath,
       headless: true,
-      ignoreHTTPSErrors: true,
+      defaultViewport: null,
+      executablePath: executablePath(),
     });
     const page = await browser.newPage();
     await page.goto(`${url}/${slug}`, {
