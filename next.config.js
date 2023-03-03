7e8b5a860dd8a88cdabbe1e6cfbfde35b7dd0bfb
hellboy41785 <unknown41785@gmail.com>
2023-03-03 20:29:20 +0530

    modified:   next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    GOOGLE_ID: process.env.GOOGLE_ID,
+    GOOGLE_SECRET: process.env.GOOGLE_SECRET,
+
+    DATABASE_URL: process.env.DATABASE_URL,
+
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+  },
 };
 
 module.exports = nextConfig;
